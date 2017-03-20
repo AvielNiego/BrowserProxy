54d47698c59dc60d50a58b3f90ee7d7276d5142a
Aviel <avielniego@gmail.com>
2017-03-21 00:10:15 +0200

    Initial commit

---
+++ yes_planet_api_webserver.py
@@ -0,0 +1,20 @@
+import time
+
+from flask import Flask
+from flask import request
+from pyvirtualdisplay import Display
+from selenium import webdriver
+
+app = Flask(__name__)
+display = Display(visible=0, size=(800, 600))
+display.start()
+
+
+@app.route("/yesplanet/api")
+def yesplanet_api():
+    driver = webdriver.Firefox()
+    driver.get(request.args.get('url'))
+    time.sleep(5)
+    source = driver.page_source
+    driver.close()
+    return source

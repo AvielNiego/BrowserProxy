f3b6c2a7378d43df4ba1f0ffe842fc7fe74d3f3a
Aviel <avielniego@gmail.com>
2017-03-21 07:33:51 +0200

    / added

--- yes_planet_api_webserver.py
+++ yes_planet_api_webserver.py
@@ -9,17 +9,17 @@ app = Flask(__name__)
 display = Display(visible=0, size=(800, 600))
 display.start()
 
-
-@app.route("/")
-def yesplanet_api():
-    return "Hi"
-
 #
-# @app.route("/yesplanet/api")
+# @app.route("/")
 # def yesplanet_api():
-#     driver = webdriver.Firefox()
-#     driver.get(request.args.get('url'))
-#     time.sleep(5)
-#     source = driver.page_source
-#     driver.close()
-#     return source
+#     return "Hi"
+
+
+@app.route("/yesplanet/api")
+def yesplanet_api():
+    driver = webdriver.Firefox()
+    driver.get(request.args.get('url'))
+    time.sleep(5)
+    source = driver.page_source
+    driver.close()
+    return source

32603317ad60cfdcc65417900ffb2b43c52b9067
Aviel <avielniego@gmail.com>
2017-04-09 00:53:16 +0300

    remove cache

--- yes_planet_api_webserver.py
+++ yes_planet_api_webserver.py
@@ -8,8 +8,6 @@ from selenium import webdriver
 from selenium.webdriver.support.wait import WebDriverWait
 
 app = Flask(__name__)
-last_updated = datetime.datetime.now()
-presentations_json = {}
 
 
 def create_new_driver():
@@ -21,13 +19,6 @@ def create_new_driver():
 
 @app.route("/yesplanet/api/presentations")
 def yesplanet_api_presentations():
-    if datetime.datetime.now() - datetime.timedelta(hours=1) > last_updated:
-        update_presentations_json()
-    return presentations_json
-
-
-def update_presentations_json():
-    global presentations_json, last_updated
     driver = create_new_driver()
     waiter = WebDriverWait(driver, 10, 0.001)
     driver.get("http://www.yesplanet.co.il/")
@@ -36,7 +27,6 @@ def update_presentations_json():
         "$.ajax({'url' : 'movies/presentationsJSON','type' : 'GET','success' : function(data) {a = data}});")
     waiter.until(lambda d: d.execute_script("return typeof a !== 'undefined'"))
     presentations_json = json.dumps(driver.execute_script("return a"))
-    last_updated = datetime.datetime.now()
     driver.close()
     return presentations_json
 

104866ca4d41fa7a818c4f6966654027b3e4af2d
Aviel <avielniego@gmail.com>
2017-04-09 00:56:32 +0300

    add yesplanet home to api

--- yes_planet_api_webserver.py
+++ yes_planet_api_webserver.py
@@ -16,6 +16,15 @@ def create_new_driver():
     chrome_options.add_experimental_option("prefs", prefs)
     return webdriver.Chrome(chrome_options=chrome_options)
 
+@app.route("/")
+def yesplanet_api_presentations():
+    driver = create_new_driver()
+    driver.get("http://www.yesplanet.co.il/")
+    WebDriverWait(driver, 10, 0.001).until(lambda d: d.find_elements_by_id("fancy_overlay"))
+    source = driver.page_source
+    driver.close()
+    return source
+
 
 @app.route("/presentationsJSON")
 def yesplanet_api_presentations():

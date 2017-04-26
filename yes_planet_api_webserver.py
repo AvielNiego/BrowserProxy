42356fe6a0702d52358ccd591fc8f38d5c2fbdba
Aviel <avielniego@gmail.com>
2017-04-26 18:03:50 +0300

    make sure driver is alive before making requests

--- yes_planet_api_webserver.py
+++ yes_planet_api_webserver.py
@@ -13,10 +13,14 @@ waiter = None
 
 
 def create_new_driver():
+    global driver, waiter
     chrome_options = webdriver.ChromeOptions()
     prefs = {"profile.managed_default_content_settings.images": 2}
     chrome_options.add_experimental_option("prefs", prefs)
-    return webdriver.Chrome(chrome_options=chrome_options)
+    driver = webdriver.Chrome(chrome_options=chrome_options)
+    waiter = WebDriverWait(driver, 10, 0.1)
+    driver.get("http://www.yesplanet.co.il/")
+    waiter.until(lambda d: d.find_elements_by_id("fancy_overlay"))
 
 
 @app.route("/")
@@ -26,10 +30,11 @@ def yesplanet_api_home():
 
 @app.route("/presentationsJSON")
 def yesplanet_api_presentations():
-    return _get_yesplanet_path("movies/presentationsJSON")
+    return get_yesplanet_path("movies/presentationsJSON")
 
 
-def _get_yesplanet_path(path):
+def get_yesplanet_path(path):
+    validate_driver()
     driver.execute_script('a = null')
     driver.execute_script(
         "$.ajax({'url' : '%s','type' : 'GET','success' : function(data) {a = data}});" % path)
@@ -37,11 +42,14 @@ def _get_yesplanet_path(path):
     return json.dumps(driver.execute_script("return a"))
 
 
+def validate_driver():
+    try:
+        driver.execute_script('1 == 1')
+    except:
+        create_new_driver()
+
+
 if __name__ == "__main__":
     display = Display(visible=0, size=(800, 600))
     display.start()
-    driver = create_new_driver()
-    waiter = WebDriverWait(driver, 10, 0.1)
-    driver.get("http://www.yesplanet.co.il/")
-    waiter.until(lambda d: d.find_elements_by_id("fancy_overlay"))
     app.run('0.0.0.0')

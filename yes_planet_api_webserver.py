dda8a0436c51019a2529afd507148008f4ae8640
Aviel <avielniego@gmail.com>
2017-04-26 18:01:01 +0300

    make requests only with js

--- yes_planet_api_webserver.py
+++ yes_planet_api_webserver.py
@@ -1,13 +1,15 @@
 # coding=utf-8
+import datetime
 import json
 
-import datetime
 from flask import Flask
 from pyvirtualdisplay import Display
 from selenium import webdriver
 from selenium.webdriver.support.wait import WebDriverWait
 
 app = Flask(__name__)
+driver = None
+waiter = None
 
 
 def create_new_driver():
@@ -16,31 +18,30 @@ def create_new_driver():
     chrome_options.add_experimental_option("prefs", prefs)
     return webdriver.Chrome(chrome_options=chrome_options)
 
+
 @app.route("/")
 def yesplanet_api_home():
-    driver = create_new_driver()
-    driver.get("http://www.yesplanet.co.il/")
-    WebDriverWait(driver, 10, 0.001).until(lambda d: d.find_elements_by_id("fancy_overlay"))
-    source = driver.page_source
-    driver.close()
-    return source
+    return driver.page_source
 
 
 @app.route("/presentationsJSON")
 def yesplanet_api_presentations():
-    driver = create_new_driver()
-    waiter = WebDriverWait(driver, 10, 0.001)
-    driver.get("http://www.yesplanet.co.il/")
-    waiter.until(lambda d: d.find_elements_by_id("fancy_overlay"))
+    return _get_yesplanet_path("movies/presentationsJSON")
+
+
+def _get_yesplanet_path(path):
+    driver.execute_script('a = null')
     driver.execute_script(
-        "$.ajax({'url' : 'movies/presentationsJSON','type' : 'GET','success' : function(data) {a = data}});")
-    waiter.until(lambda d: d.execute_script("return typeof a !== 'undefined'"))
-    presentations_json = json.dumps(driver.execute_script("return a"))
-    driver.close()
-    return presentations_json
+        "$.ajax({'url' : '%s','type' : 'GET','success' : function(data) {a = data}});" % path)
+    waiter.until(lambda d: d.execute_script("return a != null"))
+    return json.dumps(driver.execute_script("return a"))
 
 
 if __name__ == "__main__":
     display = Display(visible=0, size=(800, 600))
     display.start()
+    driver = create_new_driver()
+    waiter = WebDriverWait(driver, 10, 0.1)
+    driver.get("http://www.yesplanet.co.il/")
+    waiter.until(lambda d: d.find_elements_by_id("fancy_overlay"))
     app.run('0.0.0.0')

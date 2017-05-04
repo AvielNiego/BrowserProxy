b5b8ca4074e2864820369e2daa5f9e4a386c15e7
Aviel <avielniego@gmail.com>
2017-05-04 15:03:53 +0300

    close driver after each use

--- yes_planet_api_webserver.py
+++ yes_planet_api_webserver.py
@@ -1,5 +1,4 @@
 # coding=utf-8
-import datetime
 import json
 import random
 import string
@@ -10,12 +9,9 @@ from selenium import webdriver
 from selenium.webdriver.support.wait import WebDriverWait
 
 app = Flask(__name__)
-driver = None
-waiter = None
 
 
 def create_new_driver():
-    global driver, waiter
     chrome_options = webdriver.ChromeOptions()
     prefs = {"profile.managed_default_content_settings.images": 2}
     chrome_options.add_experimental_option("prefs", prefs)
@@ -23,12 +19,16 @@ def create_new_driver():
     waiter = WebDriverWait(driver, 10, 0.1)
     driver.get("http://www.yesplanet.co.il/")
     waiter.until(lambda d: d.find_elements_by_id("fancy_overlay"))
+    return driver
 
 
 @app.route("/")
 def yesplanet_api_home():
-    create_new_driver()
-    return driver.page_source
+    driver = create_new_driver()
+    try:
+        return create_new_driver().page_source
+    finally:
+        driver.close()
 
 
 @app.route("/presentationsJSON")
@@ -37,17 +37,18 @@ def yesplanet_api_presentations():
 
 
 def get_yesplanet_path(path):
-    create_new_driver()
     var_name = ''.join(random.choice(string.ascii_letters) for _ in range(15))
+    driver = create_new_driver()
     driver.execute_script('%s = null' % var_name)
     driver.execute_script(
         ("$.ajax({'url' : '%s','type' : 'GET','success' : function(data) {" + var_name + " = data}});") % path)
+    waiter = WebDriverWait(driver, 10, 0.1)
     waiter.until(lambda d: d.execute_script("return %s != null" % var_name))
+    driver.close()
     return json.dumps(driver.execute_script("return %s" % var_name))
 
 
 if __name__ == "__main__":
     display = Display(visible=0, size=(800, 600))
     display.start()
-    create_new_driver()
     app.run('0.0.0.0')
